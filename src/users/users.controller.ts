321b1e0844d108ff0a1bd3e334c4535ecb100c37
mehdiAkc <akkmehdi0@gmail.com>
2026-06-15 20:19:05 +0100

    nop

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,11 @@ export class UsersController {
   
   constructor(private readonly userService: UsersService){}
 
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
   @Patch()
   @UseGuards(JwtAuthGuard)
   async update(
